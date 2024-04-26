bed2ebe4f23309a3abe9a0a6d3921273b7d9b8ed
DBS <david.b@quali.com>
2024-04-26 16:16:36 +0300

    fix script

--- assets/scripts/get-url.sh
+++ assets/scripts/get-url.sh
@@ -1,7 +1,7 @@
 echo "Getting url service address"
-sleep 30s
+# sleep 30
 export RELEASE_NAME=$1
 export NAMESPACE=$2
-export url=$(kubectl get service $RELEASE_NAME -n $NAMESPACE --no-headers | awk '{print $4}')
+export url=$(kubectl get service -n $NAMESPACE | grep $RELEASE_NAME | grep LoadBalancer | awk '{print $1}' | xargs kubectl get service -n $NAMESPACE --no-headers | awk '{print $4}')
 
-echo $url
+echo url=$url
